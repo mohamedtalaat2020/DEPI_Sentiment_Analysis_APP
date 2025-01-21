57898300bde4c7a68da3db224805123a01ae6d1d
MOHAMED TALAAT <70910257+mohamedtalaat2020@users.noreply.github.com>
2025-01-21 08:05:04 +0000

    2025-v3

--- Main_Page.py
+++ Main_Page.py
@@ -24,6 +24,13 @@ st.markdown(html_temp, unsafe_allow_html=True)
 # app design
 set_bg_hack('Picture1.png')
 
+# Add an image
+st.image(
+    "Home.png",  # Replace with the path to your image
+    caption="Home Page",
+    use_column_width=True  # Automatically adjusts the image width to the column size
+)
+
 st.sidebar.header("About App")
 st.sidebar.info("A Customer Sentiment analysis Project which collect data of reviews of Amazon products. The reviews will then be used to determine the Sentiments of those reviews. \
                 The different Visualizations will help us get a feel of the overall exploration of reviews")
